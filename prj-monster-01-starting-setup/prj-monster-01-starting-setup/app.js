1a8214ed130e26af68c8245746565819ca888b37
Daniel Villanueva <dvillanuevap@indra.es>
2024-02-21 11:22:45 +0100

    Core Functionality + "Battle Log"

--- prj-monster-01-starting-setup/prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/prj-monster-01-starting-setup/app.js
@@ -8,7 +8,8 @@ const app = Vue.createApp({
             playerHealth: 100,
             monsterHealth: 100,
             currentRound: 0,
-            winner: null
+            winner: null,
+            logMessages: []
         };
     },
     methods: {
@@ -16,17 +17,20 @@ const app = Vue.createApp({
             this.currentRound++;
             const attackValue = getRandomValue(5,12);
             this.monsterHealth -= attackValue;
+            this.addLogMessage('player', 'attack', attackValue);
             this.attackPlayer();
         },
         attackPlayer(){
             const attackValue = getRandomValue(8,15);
             this.playerHealth -= attackValue;
+            this.addLogMessage('monster', 'attack', attackValue);
         },
         specialAttackMonster(){
             this.currentRound++;
             const attackValue = getRandomValue(10,25);
             this.monsterHealth -= attackValue;
             this.attackPlayer();
+            this.addLogMessage('player', 'attack', attackValue);
         },
         healPlayer(){
             this.currentRound++;
@@ -35,14 +39,38 @@ const app = Vue.createApp({
                 this.playerHealth = 100;
             else 
                 this.playerHealth += healValue;
+                this.addLogMessage('player', 'heal', healValue);
             this.attackPlayer();
+        },
+        startGame(){
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.currentRound = 0;
+            this.winner = null;
+            this.logMessages = [];
+        },
+        surrender(){
+            this.winner = "monster";
+        },
+        addLogMessage(who, what, value){
+            this.logMessages.unshift({
+                actionBy: who,
+                actionType: what,
+                actionValue: value
+            });
         }
     },
     computed:{
         monsterBarStyles(){
+            if(this.monsterHealth < 0){
+                return {width: '0%'};
+            }
             return {width: this.monsterHealth + '%'};
         },
         playerBarStyles(){
+            if(this.playerHealth < 0){
+                return {width: '0%'}
+            }
             return {width: this.playerHealth + '%'};
         },
         mayUseSpecialAttack(){
