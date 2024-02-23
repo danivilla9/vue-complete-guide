6738f21abf3b9f725bc64b80b5d8fab637e6708d
Daniel Villanueva <dvillanuevap@indra.es>
2024-02-23 09:26:41 +0100

    Project 09 - Parent->Child Comm

--- vue-cli-01-a-new-vue-project/vue-cli-01-a-new-vue-project/src/main.js
+++ vue-cli-01-a-new-vue-project/vue-cli-01-a-new-vue-project/src/main.js
@@ -1,3 +1,10 @@
 import { createApp } from 'vue';
+import FriendContact from './components/FriendContact.vue';
 
-createApp({}).mount('');
+import App from './App.vue';
+
+const app = createApp(App);
+
+app.component('friend-contact', FriendContact)
+
+app.mount('#app');
