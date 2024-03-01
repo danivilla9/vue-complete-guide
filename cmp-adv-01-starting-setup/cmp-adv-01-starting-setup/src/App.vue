00e67ab137eb00c1dd4471dcccdaee8bfb84cbfc
Daniel Villanueva <dvillanuevap@indra.es>
2024-03-01 10:36:06 +0100

    Project 11 Complete

--- cmp-adv-01-starting-setup/cmp-adv-01-starting-setup/src/App.vue
+++ cmp-adv-01-starting-setup/cmp-adv-01-starting-setup/src/App.vue
@@ -1,32 +1,40 @@
+<!-- eslint-disable vue/no-unused-components -->
 <template>
   <div>
     <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info :full-name="activeUser.name" :info-text="activeUser.description" :role="activeUser.role"></user-info>
-    <course-goals #default="slotProps">
-        <h2>{{ slotProps.item }}</h2>
-        <p>{{ slotProps['another-prop'] }}</p>
-    </course-goals>
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <!--
+    <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals>-->
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
-import TheHeader from './components/TheHeader.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
-import CourseGoals from './components/CourseGoals.vue';
+import TheHeader from './components/layout/TheHeader.vue';
+//import BadgeList from './components/BadgeList.vue';
+//import UserInfo from './components/UserInfo.vue';
+//import CourseGoals from './components/CourseGoals.vue';
+import ActiveGoals from './components/ActiveGoals.vue';
+import ManageGoals from './components/ManageGoals.vue';
 
 export default {
   components: {
-    'the-header': TheHeader,
-    // TheHeader: TheHeader //also valid
+    TheHeader,
+    // 'the-header': TheHeader //also valid
     // TheHeader //valid too
-    BadgeList,
-    UserInfo,
-    CourseGoals,
+    //BadgeList,
+    //UserInfo,
+    //CourseGoals,
+    ActiveGoals,
+    ManageGoals
   },
   data() {
     return {
+      selectedComponent: 'active-goals',
       activeUser: {
         name: 'Maximilian Schwarzmüller',
         description: 'Site owner and admin',
@@ -34,6 +42,11 @@ export default {
       },
     };
   },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+    }
+  }
 };
 </script>
 
