ea383e0603ba11d96440de651e3e2a9d2f5a38ef
Daniel Villanueva <dvillanuevap@indra.es>
2024-03-01 09:36:31 +0100

    Project 11 - Slots

--- cmp-adv-01-starting-setup/cmp-adv-01-starting-setup/src/App.vue
+++ cmp-adv-01-starting-setup/cmp-adv-01-starting-setup/src/App.vue
@@ -2,16 +2,29 @@
   <div>
     <the-header></the-header>
     <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
+    <user-info :full-name="activeUser.name" :info-text="activeUser.description" :role="activeUser.role"></user-info>
+    <course-goals #default="slotProps">
+        <h2>{{ slotProps.item }}</h2>
+        <p>{{ slotProps['another-prop'] }}</p>
+    </course-goals>
   </div>
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
+import CourseGoals from './components/CourseGoals.vue';
+
 export default {
+  components: {
+    'the-header': TheHeader,
+    // TheHeader: TheHeader //also valid
+    // TheHeader //valid too
+    BadgeList,
+    UserInfo,
+    CourseGoals,
+  },
   data() {
     return {
       activeUser: {
